542f8cf3a35774756b968fc6a8f10a50f491f8d2
Petro Abramov <sebshe3@gmail.com>
2023-06-11 00:18:08 +0300

    Created MainView in views.py for kitchen_service/index.html

--- kitchen_service/views.py
+++ kitchen_service/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from kitchen_service.models import DishType, Dish, Cook
+
+
+class MainView(TemplateView):
+    template_name = 'kitchen_service/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(MainView, self).get_context_data(**kwargs)
+        context['dish_types_count'] = DishType.objects.count()
+        context['dishes_count'] = Dish.objects.count()
+        context['cooks_count'] = Cook.objects.count()
+        return context
