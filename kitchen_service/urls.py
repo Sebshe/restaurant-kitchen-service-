87a28dc8571121f7f7d8035688fb9409f8d6b4bd
Petro Abramov <sebshe3@gmail.com>
2023-06-11 21:42:45 +0300

    Populated code for type of dish form & added path for type of dish form

--- kitchen_service/urls.py
+++ kitchen_service/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
-from .views import MainView, DishTypeListView
+from .views import (
+    MainView,
+    DishTypeListView,
+    DishTypeCreateView
+)
 
 urlpatterns = [
     path('', MainView.as_view(), name='index'),
@@ -8,6 +12,11 @@ urlpatterns = [
         'types_of_dish/',
         DishTypeListView.as_view(),
         name='types-of-dish'
+    ),
+    path(
+        'types_of_dish/create/',
+        DishTypeCreateView.as_view(),
+        name='types-of-dish-create'
     ),
 ]
 
