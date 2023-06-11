7e85574712bd9904c31f634981774892687d2ac9
Petro Abramov <sebshe3@gmail.com>
2023-06-11 19:23:16 +0300

    Populated code for displaying dish types in a table in dish_type_list.html & added path in urlpatterns

--- kitchen_service/urls.py
+++ kitchen_service/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import MainView
-
+from .views import MainView, DishTypeListView
 
 urlpatterns = [
     path('', MainView.as_view(), name='index'),
+    path(
+        'types_of_dish/',
+        DishTypeListView.as_view(),
+        name='types-of-dish'
+    ),
 ]
 
 app_name = "kitchen_service"
